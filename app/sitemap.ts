84ebc1c2ba8d616ca393e298b51be6c1bf68f24f
Owura Opoku <opokujay45@gmail.com>
2026-03-22 03:08:31 +0000

    Sitemap added

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,8 @@
-import type { MetadataRoute } from "next";
-
-export default function sitemap(): MetadataRoute.Sitemap {
+export default function sitemap() {
   return [
     {
-      url: "https://ovalhome.vercel.app",
+      url: "https://ovalhome.vercel.app/",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
     },
   ];
 }
